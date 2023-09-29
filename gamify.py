72814f0f9aed60e7a92a0a81a52c454de10e4c16
Enahtem <enahtem.ch4@gmail.com>
2023-09-29 16:32:01 -0400

    test

--- gamify.py
+++ gamify.py
@@ -1,10 +1,15 @@
+def perform_activity(activity_name, time):
+    return
+
 def initialize():
     # global variables here
-    health_point = 0
+    global health_points, hedons, is_running, is_carrying_textbooks, is_resting
+    health_points = 0
     hedons = 0
     is_running = False
     is_carrying_textbooks = False
     is_resting = False
 
 if __name__ == "__main__":
+    initialize()
     pass
